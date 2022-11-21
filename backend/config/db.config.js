3deaa2beb0ea3c6bf206c4160b5c735d47b540d3
Huiyeong <53117014+klloo@users.noreply.github.com>
2022-11-21 20:30:37 +0900

    Update db.config.js

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "h1use0ulyws4lqr1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    USER: "ifgx9brx1ezlgmst",
-    PASSWORD: "itvxbruaw6alyrg5",
-    DB: "fogv45qvmxfr3j5f"
+    HOST: "",
+    USER: "",
+    PASSWORD: "",
+    DB: ""
 };
